b35a555dfb62a114c0d316be4d2cda08709dccd6
ARYA_DEEP <deepesharya82246@gmail.com>
2019-11-06 18:46:15 +0530

    initial

---
+++ css/gym3.js
@@ -0,0 +1,270 @@
+function sho1(){
+    document.getElementById("nav1").innerHTML= "In one line simply Health is wealth ";
+}
+function hid1(){
+    document.getElementById("nav1").innerHTML="Why to join gym or yoga?"
+}
+function sho2(){
+    document.getElementById("nav2").innerHTML= "We provide instructor for our every trainee in low cost.";
+}
+function hid2(){
+    document.getElementById("nav2").innerHTML="Why us?"
+}
+function sho3(){
+    document.getElementById("nav3").innerHTML= "23561452";
+}
+function hid3(){
+    document.getElementById("nav3").innerHTML="contact us"
+}
+
+//following code is for showing firs image
+
+$(function vr1(){
+   $("img").hide(); 
+});
+
+function shy1(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Adho%20_Mukha_Svanasana.png";
+    document.getElementById("yo1").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy1(){
+   $("img").hide();
+}
+
+//following code is for second image
+
+$(function vr2(){
+   $("img").hide(); 
+});
+
+function shy2(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Bakasana.png";
+    document.getElementById("yo2").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy2(){
+   $("img").hide();
+}
+
+//following code is for third image
+
+$(function vr3(){
+   $("img").hide(); 
+});
+
+function shy3(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Balasana.png";
+    document.getElementById("yo3").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy3(){
+   $("img").hide();
+}
+
+
+//following code is for fourth image
+
+$(function vr4(){
+   $("img").hide(); 
+});
+
+function shy4(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Bandha%20Sarvangasana.png" ;
+    document.getElementById("yo4").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy4(){
+   $("img").hide();
+}
+
+//following code is for fifth image
+
+$(function vr5(){
+   $("img").hide(); 
+});
+
+function shy5(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Chaturanga.png";
+    document.getElementById("yo5").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy5(){
+   $("img").hide();
+}
+
+//following code is for sixth image
+
+$(function vr6(){
+   $("img").hide(); 
+});
+
+function shy6(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Natarajasana.png" ;
+    document.getElementById("yo6").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy6(){
+   $("img").hide();
+}
+//following code is for seventh image
+
+$(function vr7(){
+   $("img").hide(); 
+});
+
+function shy7(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Navasana.png" ;
+    document.getElementById("yo7").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy7(){
+   $("img").hide();
+}
+
+//following code is for eighth image
+
+$(function vr6(){
+   $("img").hide(); 
+});
+
+function shy8(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Pincha%20Mayurasana.png" ;
+    document.getElementById("yo8").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy8(){
+   $("img").hide();
+}
+
+//following code is for ninth image
+
+$(function vr9(){
+   $("img").hide(); 
+});
+
+function shy9(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Trikonasana.png" ;
+    document.getElementById("yo9").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy9(){
+   $("img").hide();
+}
+
+//following code is for tenth image
+
+$(function vr10(){
+   $("img").hide(); 
+});
+
+function shy10(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Utkatasana.png" ;
+    document.getElementById("yo10").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy10(){
+   $("img").hide();
+}
+
+//following code is for eleventh image
+
+$(function vr11(){
+   $("img").hide(); 
+});
+
+function shy11(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Virabhadrasana%20I.png" ;
+    document.getElementById("yo11").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy11(){
+   $("img").hide();
+}
+
+//following code is for twelth image
+
+$(function vr12(){
+   $("img").hide(); 
+});
+
+function shy12(){
+var im1 = document.createElement("img");
+    im1.src = "./images/Virabhadrasana%20II.png" ;
+    document.getElementById("yo12").appendChild(im1).style.no-repeat;
+    
+}
+
+function hiy12(){
+   $("img").hide();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
